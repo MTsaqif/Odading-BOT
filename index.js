9276a71a5ab5daaaf85ee2159fb6df97ade178ca
aditiapribadiansyah <74158274+aditiapribadiansyah@users.noreply.github.com>
2020-11-09 22:54:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -630,6 +630,35 @@ if (text.includes("!pokemon"))
     });
     }
 
+if (text.includes("!dragonball"))
+   {
+    var items = ["dragonball"];
+    var nime = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
+    
+    axios.get(url)
+      .then((result) => {
+        var n = JSON.parse(JSON.stringify(result.data));
+        var nimek =  n[Math.floor(Math.random() * n.length)];
+        imageToBase64(nimek) 
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); 
+              conn.sendMessage(
+            id,
+              buf,MessageType.image)
+       
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error);
+            }
+        )
+    
+    });
+    }
+
 if (text.includes("!loli"))
    {
     var items = ["anime loli"];
