5a00ad162d33da6e5d196fd01745aceb4f4b963c
aditiapribadiansyah <74158274+aditiapribadiansyah@users.noreply.github.com>
2020-11-10 02:34:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -596,6 +596,34 @@ if (text.includes("!animepict"))
     
     });
     }
+if (text.includes("!hentai"))
+   {
+    var items = ["hentai"];
+    var nime = items[Math.floor(Math.random() * items.length)];
+    var url = "https://api.fdci.se/rep.php?gambar=" + nime;
+    
+    axios.get(url)
+      .then((result) => {
+        var n = JSON.parse(JSON.stringify(result.data));
+        var nimek =  n[Math.floor(Math.random() * n.length)];
+        imageToBase64(nimek) 
+        .then(
+            (response) => {
+	var buf = Buffer.from(response, 'base64'); 
+              conn.sendMessage(
+            id,
+              buf,MessageType.image)
+       
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error);
+            }
+        )
+    
+    });
+    }
 
 if (text.includes("!pokemon"))
    {
